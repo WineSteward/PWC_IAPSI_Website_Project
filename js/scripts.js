3ede9671750dd7458d581dd4409be8d0fe731172
WineSteward <miguel2001carpinteiro@hotmail.com>
2024-01-19 14:44:29 +0000

    So falta a tabela dos favoritos e depois entrega

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,7 @@ var current_page = 1;
 var token;
 var clone_card_cao;
 const element_spinner_wrapper = document.querySelector(".spinner-wrapper");
-
-function popup_info(nome, id_quantidade, preco) {
-
-    var quantidade_produto = document.getElementById(id_quantidade).value;
-
-    var preco_numero = parseFloat(preco);
-    var quantidade_numero = parseFloat(quantidade_produto);
-    var total = preco_numero * quantidade_numero;
-    var total_string = String(total);
-
-        if (quantidade_produto > "0" || quantidade_produto !== "") {
-            $('#myModal').modal('show');
-            $("#nome_produto_popup").text(nome);
-            $("#quantidade_produto_popup").text(quantidade_produto);
-            $("#preco_final").text(total_string);
-        }
-        else {
-            alert("Não foi escolhida nenhuma quantidade");
-            $('#myModal').modal('hide');
-        }
-
-
-}
-
-function reset_page_store(){
-    location.reload();
-}
+var objeto_cao;
 
 document.addEventListener('DOMContentLoaded', function () {
     clone_card_cao = $(".card-cao").clone(); //clonar o card
@@ -55,6 +29,33 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+function popup_info(nome, id_quantidade, preco) {
+
+    var quantidade_produto = document.getElementById(id_quantidade).value;
+
+    var preco_numero = parseFloat(preco);
+    var quantidade_numero = parseFloat(quantidade_produto);
+    var total = preco_numero * quantidade_numero;
+    var total_string = String(total);
+
+    if (quantidade_produto > "0" || quantidade_produto !== "") {
+        $('#myModal').modal('show');
+        $("#nome_produto_popup").text(nome);
+        $("#quantidade_produto_popup").text(quantidade_produto);
+        $("#preco_final").text(total_string);
+    }
+    else {
+        alert("Não foi escolhida nenhuma quantidade");
+        $('#myModal').modal('hide');
+    }
+
+
+}
+
+function reset_page_store() {
+    location.reload();
+}
+
 
 function ir_detalhes_cao(id_cao) {
     localStorage.setItem("key_id", id_cao);
@@ -64,19 +65,18 @@ function ir_detalhes_cao(id_cao) {
 
 function ver_detalhes_cao() {
 
-    var id_cao = localStorage.getItem("key_id");
-
-    console.log(id_cao);
+    var id_cao_detalhes = localStorage.getItem("key_id");
 
     $.ajax({
 
         method: 'GET',
-        url: `https://api.petfinder.com/v2/animals/${id_cao}`,
+        url: `https://api.petfinder.com/v2/animals/${id_cao_detalhes}`,
         headers: { "Authorization": "Bearer " + token }
 
     }).done(function (dados_recebidos) {
 
         element_spinner_wrapper.style.display = "none";
+
         //console.log(dados_recebidos);
 
         $("#nome_cao_detalhes").text(dados_recebidos.animal.name);
@@ -91,32 +91,82 @@ function ver_detalhes_cao() {
 
         $("#raca_cao_detalhes").text(dados_recebidos.animal.breeds.primary);
 
-        if (dados_recebidos.animal.primary_photo_cropped.large != null) {
+        if (dados_recebidos.animal.primary_photo_cropped != null) {
             $("#imagem_cao_detalhes").attr("src", dados_recebidos.animal.primary_photo_cropped.full); //Caso tenha imagem fica com a imagem que tem se nao usamos uma stock
         }
         else {
             $("#imagem_cao_detalhes").attr("src", "images/dog_2.jpg");
         }
+
+        for (var i = 0; i < localStorage.length; i++) {
+            var key = localStorage.key(i);
+            if (dados_recebidos.animal.id == key) {
+                $("#imagem_badge_favorito").attr("src", "images/favorito_on.png");
+                break;
+            }
+            else {
+                $("#imagem_badge_favorito").attr("src", "images/favorito_off.png");
+            }
+        }
+
+        $("#botao_favorito").click(function () {
+            objeto_cao =
+            {
+                "Nome": dados_recebidos.animal.name,
+                "Raca": dados_recebidos.animal.breeds.primary,
+                "Idade": dados_recebidos.animal.age,
+                "Genero": dados_recebidos.animal.gender,
+                "Identificador": dados_recebidos.animal.id,
+                "Estado": ""
+            }
+
+            var key = dados_recebidos.animal.id;
+            var jafiz = false;
+
+            for (var i = 0; i < localStorage.length; i++) {
+                var storedKey = localStorage.key(i);
+
+                if (key == storedKey) {
+                    // Esta nos favoritos entao remover
+                    $("#imagem_badge_favorito").attr("src", "images/favorito_off.png");
+                    objeto_cao.Estado = "Nao Favorito";
+                    localStorage.removeItem(key);
+                    jafiz = true;
+                    break;
+                }
+            }
+
+            if (!jafiz) {
+                // Nao esta nos favoritos entao adiciono
+                $("#imagem_badge_favorito").attr("src", "images/favorito_on.png");
+                objeto_cao.Estado = "Favorito";
+                var cao_favorito_string = JSON.stringify(objeto_cao);
+                localStorage.setItem(key, cao_favorito_string);
+            }
+        });
+
     });
 }
 
+
+
 function obterToken() {
 
-    // Configurando os dados para a solicitação
+    // Configurar os dados para o pedido
     const requestData = {
         grant_type: 'client_credentials',
         client_id: "IWbfaErC1hu1jUBriWKk2NyHOL0BlpOSAoXjkeoSPdoxT39bdt",
         client_secret: "qKjK2DtpDnjey7lAhQJ2iLEaMHaVQcHIPSTwBOzz"
     };
 
-    // Configurando as opções da solicitação Fetch
+    // Configuraçoes Fetch
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: new URLSearchParams(requestData)
     };
 
-    // Fazendo a solicitação para obter o token
+    // Pedido do token
     return fetch("https://api.petfinder.com/v2/oauth2/token", requestOptions)
         .then(response => response.json())
         .then(data => data.access_token)
@@ -129,7 +179,7 @@ function obterToken() {
 
 function atualizarCardsComToken(current_page) {
 
-    // Fazendo uma solicitação AJAX para obter informações sobre animais (cães)
+    // Pedido AJAX a API com token
     $.ajax({
 
         method: 'GET',
@@ -144,7 +194,7 @@ function atualizarCardsComToken(current_page) {
 
         $.each(dados_recebidos.animals, function (indice, result) {
 
-            console.log(result);
+            //console.log(result);
 
             var card = clone_card_cao.clone(); // replicamos um card novo para nao sobrepor informaçoes e perder o original
 
@@ -158,6 +208,33 @@ function atualizarCardsComToken(current_page) {
 
             $(".adicionar_cao_id", card).attr("onclick", `ir_detalhes_cao(${result.id})`);
 
+            $(".image_cao_id", card).attr("onclick", `ir_detalhes_cao(${result.id})`);
+
+            for (var i = 0; i < localStorage.length; i++) {
+                var key = localStorage.key(i);
+
+                if (result.id == key) {
+                    ;
+                    $(".botao_cao_favorito", card).addClass("favorito");
+                }
+            }
+
+            (function (objeto_cao) { //funcao anonima
+                $(".botao_cao_favorito", card).click(function () {
+                    var botaoFavorito = $(this).closest('.card-cao').find('.botao_cao_favorito');
+                    manipular_cao_favorito(objeto_cao, botaoFavorito);//nao preciso converter para string
+                });
+            })({
+                "Nome": result.name,
+                "Raca": result.breeds.primary,
+                "Idade": result.age,
+                "Genero": result.gender,
+                "Identificador": result.id,
+                "Estado": ""
+            });
+
+
+
             if (result.photos.large != null) {
                 $(".cao-imagem", card).attr("src", result.photos.large); //Caso a tenho imagem expomos a imagem recebida 
             }
@@ -168,30 +245,46 @@ function atualizarCardsComToken(current_page) {
                 $(".cao-imagem", card).attr("src", "images/dog_1.jpg");
             }
 
-
             $(".lista-caes").append(card); //adicionamos o card novo para o fim da row
-
-
-            // A PARTIR DAQUI E PARA GUARDAR OS DADOS NO LOCAL STORAGE PARA DEPOIS QUANDO CLICAR NO DETALHES DO CAO
-            // SO APARECER UM CARD ONDE REPOMOS A INFORMACAO COM A QUE O USER CLICOU
-            //aka QUAND CLICA SALVA OS DADOS NUM OBJETO E DEPOIS LEVAMOS ESSE DADOS PARA O CARD ISOLADO NA PAG DOS DETALHES
-
-
-            var objFav = {
-                "Nome": result.name,
-                "Raca": result.primary,
-                "Descricao": result.description,
-                "Genero": result.gender,
-                "Imagem": result.photos.large
-            };
-
-            var favoritos = JSON.stringify(objFav);
-            $(".btn-favoritos", card).attr("onclick", "addFavorito(" + favoritos + ")");
-
-
         });
     });
 }
+
+
+function manipular_cao_favorito(cao_favorito, botaoFavorito) {
+    var encontrado = false;
+
+    // Verificar se a localStorage está vazia
+    if (localStorage.length === 0) {
+        cao_favorito.Estado = "Favorito";
+        var cao_favorito_string = JSON.stringify(cao_favorito);
+        localStorage.setItem(cao_favorito.Identificador, cao_favorito_string);
+        botaoFavorito.addClass("favorito");
+    } else {
+        // Procurar na localStorage
+        for (var i = 0; i < localStorage.length; i++) {
+            var key = localStorage.key(i);
+
+            if (cao_favorito.Identificador == key) {
+                encontrado = true;
+                cao_favorito.Estado = "Nao Favorito";
+                botaoFavorito.removeClass("favorito");
+                localStorage.removeItem(cao_favorito.Identificador);
+                break; // Saia do loop assim que encontrar o cão
+            }
+        }
+
+        // Se o cão não foi encontrado adicionar na localStorage
+        if (!encontrado) {
+            cao_favorito.Estado = "Favorito";
+            var cao_favorito_string = JSON.stringify(cao_favorito);
+            localStorage.setItem(cao_favorito.Identificador, cao_favorito_string);
+            botaoFavorito.addClass("favorito");
+        }
+    }
+}
+
+
 
 function anteriorPagina() {
     current_page--;
